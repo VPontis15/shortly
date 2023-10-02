98b7243b8059b62117c12b452d1b9d22ecef698c
VPontis15 <94546957+VPontis15@users.noreply.github.com>
2023-10-02 04:22:21 +0300

    fixed nav on mobile

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const navBtn = document.querySelector(".navBtn");
+const navbar = document.querySelector(".navbar");
+
+navBtn.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
